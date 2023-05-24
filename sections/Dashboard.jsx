d92909b8ba8b7fed25a467199a0f2ca4e0f8bb75
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-24 17:58:12 +0530

    Update Dashboard.jsx

--- sections/Dashboard.jsx
+++ sections/Dashboard.jsx
@@ -25,6 +25,7 @@ const Dashboard = () => {
   });
   const [totalCoinReceivable, setTotalCoinReceivable] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [displayPlan, setdisplayPlan] = useState();
 
   const plans = [
     {
@@ -107,6 +108,7 @@ const Dashboard = () => {
 
     const result = await getUser.json();
     const data = result.user.commissionDetail;
+    setdisplayPlan(result.plans[result.plans.length - 1])
     setUserPlans(result.plans)
     setUserDetails(result.user)
     console.log(result.user);
@@ -140,7 +142,6 @@ const Dashboard = () => {
 
     setIsLoading(false);
   }
-
   //filter array
 
   var removePlan = userPlans;
@@ -165,134 +166,136 @@ const Dashboard = () => {
   return (
     <>
       <div className="bg-primary-black" style={{ width: '100%', height: 'auto' }}>
-        <div className="loader" style={isLoading ? { visibility: "visible" } : { visibility: "hidden" }}>
+        {isLoading ? <><div className="loader">
           <span class="spinner spinner--quarter"></span>
-        </div>
-        <div className="relative">
-          <div className="gradient-02 z-0" />
-          <div className="navbar">
-            <motion.nav
-              initial="hidden"
-              whileInView="show"
-              style={{ paddingTop: "2rem" }}
-              className={`${styles.xPaddings} relative`}
-            >
-              <div className={active ? "navbar-slider showw" : "navbar-slider"}>
-                <div className={active ? "slider show" : "slider"}>
-                  <div className="close" onClick={() => { setActive(!active) }} style={{ marginBottom: "3rem" }}>
-                    <svg style={{ cursor: "pointer" }} xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" viewBox="0 0 24 24" fill="none">
-                      <path d="M19 5L5 19M5.00001 5L19 19" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+        </div></> : <>
+          <div className="relative">
+            <div className="gradient-02 z-0" />
+            <div className="navbar">
+              <motion.nav
+                initial="hidden"
+                whileInView="show"
+                style={{ paddingTop: "2rem" }}
+                className={`${styles.xPaddings} relative`}
+              >
+                <div className={active ? "navbar-slider showw" : "navbar-slider"}>
+                  <div className={active ? "slider show" : "slider"}>
+                    <div className="close" onClick={() => { setActive(!active) }} style={{ marginBottom: "3rem" }}>
+                      <svg style={{ cursor: "pointer" }} xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" viewBox="0 0 24 24" fill="none">
+                        <path d="M19 5L5 19M5.00001 5L19 19" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                      </svg>
+                    </div>
+                    <ul>
+                      {/* <li>More Information</li> */}
+                      {/* <li>Withdrawal Page</li> */}
+                      <li style={userDetails.currentPlans == 0 ? { marginBottom: "2rem" } : { display: 'none' }} onClick={() => { push(`/purchase?email=${userDetails.email}&referredBy=${localStorage.getItem("refer")}&name=${userDetails.name}&id=guest`) }}>SEED ROUND</li>
+                      <li onClick={() => { handleLogout() }}>Log Out</li>
+                    </ul>
                   </div>
-                  <ul>
-                    {/* <li>More Information</li> */}
-                    {/* <li>Withdrawal Page</li> */}
-                    <li style={userDetails.currentPlans == 0 ? { marginBottom: "2rem" } : { display: 'none' }} onClick={() => { push(`/purchase?email=${userDetails.email}&referredBy=${localStorage.getItem("refer")}&name=${userDetails.name}&id=guest`) }}>SEED ROUND</li>
-                    <li onClick={() => { handleLogout() }}>Log Out</li>
-                  </ul>
                 </div>
-              </div>
-              <div className="absolute w-[50%] inset-0 gradient-01" />
-              <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-                <img
-                  src="/1.png"
-                  alt="logo"
-                  className="w-[3rem] h-[3rem] object-contain"
-                  style={{ cursor: "pointer" }}
-                />
-                <div></div>
-                <img
-                  src="/menu.svg"
-                  alt="menu"
-                  className="w-[24px] h-[24px] object-contain"
-                  style={{ cursor: "pointer" }}
-                  onClick={() => { setActive(!active) }}
-                />
-              </div>
-            </motion.nav>
+                <div className="absolute w-[50%] inset-0 gradient-01" />
+                <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+                  <img
+                    src="/1.png"
+                    alt="logo"
+                    className="w-[3rem] h-[3rem] object-contain"
+                    style={{ cursor: "pointer" }}
+                  />
+                  <div></div>
+                  <img
+                    src="/menu.svg"
+                    alt="menu"
+                    className="w-[24px] h-[24px] object-contain"
+                    style={{ cursor: "pointer" }}
+                    onClick={() => { setActive(!active) }}
+                  />
+                </div>
+              </motion.nav>
 
-            <motion.nav
-              variants={navVariants}
-              initial="hidden"
-              whileInView="show"
-              className={`${styles.xPaddings} py-8 relative`}
-            >
-              <div className="error_msg" style={{ marginBottom: "3rem" }}>
-                <div class="nft com-size">
-                  <div class='creator'>
-                    <div className='first'>
-                      <p className='subtitle'>Locked Profit will be unlock soon.</p>
+              <motion.nav
+                variants={navVariants}
+                initial="hidden"
+                whileInView="show"
+                className={`${styles.xPaddings} py-8 relative`}
+              >
+                <div className="error_msg" style={{ marginBottom: "3rem" }}>
+                  <div class="nft com-size">
+                    <div class='creator'>
+                      <div className='first'>
+                        <p className='subtitle'>Locked Profit will be unlock soon.</p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="dashboard_title">
-                <div>
-                  <h1>Welcome</h1>
-                  <h2>{userDetails.name}</h2>
+                <div className="dashboard_title">
+                  <div>
+                    <h1>Welcome</h1>
+                    <h2>{userDetails.name} <p className='subtitle' style={{ marginTop: "0", color: "#71ff92" }}>[{displayPlan}]</p></h2>
+                  </div>
+
+                  <div class="nft">
+                    <div class='creator'>
+                      <div className='first'>
+                        <p className='subtitle'>Max Receivable GODOT</p>
+                        <p className='coin'>{totalCoinReceivable + userDetails.currentBalance}</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
 
                 <div class="nft">
                   <div class='creator'>
                     <div className='first'>
-                      <p className='subtitle'>Max Receivable GODOT</p>
-                      <p className='coin'>{totalCoinReceivable + userDetails.currentBalance}</p>
+                      <p className='subtitle'>Share your referral link and start earning GODOT.</p>
+                    </div>
+                    <div className='second flex flex-row justify-between'>
+                      <p className="code">{userDetails.referCode}</p>
+                      <p className="btn-grad" onClick={() => { navigator.clipboard.writeText(`www.godotnetwork.com/register?id=${userDetails.referCode}`) }} >Copy Invite Link</p>
                     </div>
                   </div>
                 </div>
-              </div>
+              </motion.nav>
 
-              <div class="nft">
-                <div class='creator'>
-                  <div className='first'>
-                    <p className='subtitle'>Share your referral link and start earning GODOT.</p>
+              <motion.nav
+                variants={navVariants}
+                initial="hidden"
+                whileInView="show"
+                className={`${styles.xPaddings} py-8 relative`}
+              >
+                <div className="dashboard_title mt-1 mb-7">
+                  <h2 style={{ fontSize: "2rem" }}>Your stats</h2>
+                </div>
+                <div className="stats_card">
+                  <div className="card-0 common">
+                    <p className="count">{userDetails.currentBalance}</p>
+                    <p className="card_title">GDOTS</p>
                   </div>
-                  <div className='second flex flex-row justify-between'>
-                    <p className="code">{userDetails.referCode}</p>
-                    <p className="btn-grad" onClick={() => { navigator.clipboard.writeText(`www.godotnetwork.com/register?id=${userDetails.referCode}`) }} >Copy Invite Link</p>
+                  <div className="card-1 common">
+                    <p className="count">{(userDetails.currentBalance - 10000) / 1000}</p>
+                    <p className="card_title">Team</p>
+                  </div>
+                  <div className="card-2 common">
+                    <p className="count">$ {userDetails.paidBalance}</p>
+                    <p className="card_title">GDOT Received</p>
+                  </div>
+                  <div className="card-3 common">
+                    <p className="count">$ {(Math.abs(totalAMT - lockTotalTempAMT - userDetails.paidBalance)).toFixed(2)}</p>
+                    <p className="card_title">Profits</p>
+                  </div>
+                  <div className="card-4 common">
+                    <p className="count">$ {lockTotalTempAMT.toFixed(2)}</p>
+                    <p className="card_title">Locked Profits</p>
                   </div>
                 </div>
-              </div>
-            </motion.nav>
-
-            <motion.nav
-              variants={navVariants}
-              initial="hidden"
-              whileInView="show"
-              className={`${styles.xPaddings} py-8 relative`}
-            >
-              <div className="dashboard_title mt-1 mb-7">
-                <h2 style={{ fontSize: "2rem" }}>Your stats</h2>
-              </div>
-              <div className="stats_card">
-                <div className="card-0 common">
-                  <p className="count">{userDetails.currentBalance}</p>
-                  <p className="card_title">GDOTS</p>
-                </div>
-                <div className="card-1 common">
-                  <p className="count">{(userDetails.currentBalance - 10000) / 1000}</p>
-                  <p className="card_title">Team</p>
-                </div>
-                <div className="card-2 common">
-                  <p className="count">$ {userDetails.paidBalance}</p>
-                  <p className="card_title">GDOT Received</p>
-                </div>
-                <div className="card-3 common">
-                  <p className="count">$ {(Math.abs(totalAMT - lockTotalTempAMT - userDetails.paidBalance)).toFixed(2)}</p>
-                  <p className="card_title">Profits</p>
-                </div>
-                <div className="card-4 common">
-                  <p className="count">$ {lockTotalTempAMT.toFixed(2)}</p>
-                  <p className="card_title">Locked Profits</p>
-                </div>
-              </div>
-            </motion.nav>
+              </motion.nav>
+            </div>
           </div>
-        </div>
+        </>}
       </div>
     </>
   )
 }
 
 export default Dashboard
+
