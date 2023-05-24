196438958b8d45b9559d5d5e1d2105f610417bda
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-24 17:07:36 +0530

    Add files via upload

---
+++ pages/email.js
@@ -0,0 +1,12 @@
+import { Email } from '../sections';
+
+const email = () => (
+  <div className="purchase-page bg-primary-black overflow-hidden" style={{ width: '100%', height: '100vh'}} >
+    <div className="relative">
+      <div className="gradient-03 z-0" />
+      <Email />
+    </div>
+  </div>
+);
+
+export default email;
