3fa08b440479e3d8f0e4b88bbba64333c1d963c5
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-24 17:05:26 +0530

    Update Purchase.jsx

--- sections/Purchase.jsx
+++ sections/Purchase.jsx
@@ -82,7 +82,7 @@ const Purchase = () => {
                             <path id="Vector" d="M9 9V6.2002C9 5.08009 9 4.51962 9.21799 4.0918C9.40973 3.71547 9.71547 3.40973 10.0918 3.21799C10.5196 3 11.0801 3 12.2002 3H17.8002C18.9203 3 19.4801 3 19.9079 3.21799C20.2842 3.40973 20.5905 3.71547 20.7822 4.0918C21.0002 4.51962 21.0002 5.07967 21.0002 6.19978V11.7998C21.0002 12.9199 21.0002 13.48 20.7822 13.9078C20.5905 14.2841 20.2839 14.5905 19.9076 14.7822C19.4802 15 18.921 15 17.8031 15H15M9 9H6.2002C5.08009 9 4.51962 9 4.0918 9.21799C3.71547 9.40973 3.40973 9.71547 3.21799 10.0918C3 10.5196 3 11.0801 3 12.2002V17.8002C3 18.9203 3 19.4801 3.21799 19.9079C3.40973 20.2842 3.71547 20.5905 4.0918 20.7822C4.5192 21 5.07899 21 6.19691 21H11.8036C12.9215 21 13.4805 21 13.9079 20.7822C14.2842 20.5905 14.5905 20.2839 14.7822 19.9076C15 19.4802 15 18.921 15 17.8031V15M9 9H11.8002C12.9203 9 13.4801 9 13.9079 9.21799C14.2842 9.40973 14.5905 9.71547 14.7822 10.0918C15 10.5192 15 11.079 15 12.1969L15 15" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </g>
                     </svg></span></div>
-                    <p className="disclaimer">Note: Please send only BUSD to this wallet.</p>
+                    <p className="disclaimer">Note: Please copy wallet address and send only BUSD to this wallet.</p>
                     <p className="btn-grad" onClick={() => { setisOpen(1) }}>Continue</p>
                 </div>
             </div>
@@ -121,32 +121,38 @@ const Purchase = () => {
                 title={<>SEED ROUND ALLOCATION</>}
                 textStyles="text-center"
             />
+            <h2 style={{color: "#fff", fontSize: "1.2rem", margin: "2rem auto", marginBottom: "-3rem", textAlign: "center"}}>Select your desired amount and click on the <b>PURCHASE</b> button at the bottom of the page. </h2>
             <div className="stats_card" style={{ marginTop: '8rem' }}>
                 <div className="card-1 common" onClick={() => { setindex(0); setPlan("Basic"); setRate(20); }} style={index == 0 ? { height: '25vh' } : { height: '25vh', opacity: '0.2' }}>
                     <p className="card_title">Basic</p>
                     <p className="count">20 BUSD</p>
+                    <p>Eligible for level 1 reward</p>
                 </div>
                 <div className="card-4 common" onClick={() => { setindex(1); setPlan("Bronze"); setRate(50); }} style={index == 1 ? { height: '25vh' } : { height: '25vh', opacity: '0.2' }}>
                     <p className="card_title">Bronze</p>
                     <p className="count">50 BUSD</p>
+                    <p>Eligible for level upto 2 reward</p>
                 </div>
                 <div className="card-0 common" onClick={() => { setindex(2); setPlan("Silver"); setRate(100); }} style={index == 2 ? { height: '25vh' } : { height: '25vh', opacity: '0.2' }}>
                     <p className="card_title">Silver</p>
                     <p className="count">100 BUSD</p>
+                    <p>Eligible for level upto 3 reward</p>
                 </div>
                 <div className="card-4 common" onClick={() => { setindex(3); setPlan("Gold"); setRate(200); }} style={index == 3 ? { height: '25vh' } : { height: '25vh', opacity: '0.2' }}>
                     <p className="card_title">Gold</p>
                     <p className="count">200 BUSD</p>
+                    <p>Eligible for level upto 4 reward</p>
                 </div>
                 <div className="card-2 common" onClick={() => { setindex(4); setPlan("Platinum"); setRate(500); }} style={index == 4 ? { height: '25vh' } : { height: '25vh', opacity: '0.2' }}>
                     <p className="card_title">Platinum</p>
                     <p className="count">500 BUSD</p>
+                    <p>Eligible for level upto 5 reward</p>
                 </div>
             </div>
 
             <p className="btn-grad purchase" style={{ width: "20rem", height: "5rem", fontSize: "1.2rem" }} onClick={() => { index > -1 ? setisOpen(0) : alert("Please select a plan...") }}>Purchase</p>
 
-            {router.query.id == "guest" ? <></> : <p className="btn-grad purchase" style={{ marginTop: "2rem", backgroundImage: "linear-gradient(to right, #1FA2FF 0%, #12D8FA  51%, #1FA2FF  100%)" }} onClick={() => { handleSignInAsGeust() }}>Enter For FREE</p>}
+            {router.query.id == "guest" ? <></> : <p className="btn-grad purchase" style={{ marginTop: "2rem" }} onClick={() => { handleSignInAsGeust() }}>Sign In as a Guest</p>}
         </section>
     );
 };
