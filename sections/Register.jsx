4245cba71e3ee4e5bd8020f26e14c6c5928e7bab
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-25 23:22:21 +0530

    Update Register.jsx

--- sections/Register.jsx
+++ sections/Register.jsx
@@ -19,47 +19,50 @@ const Register = () => {
     const [isLoading, setIsLoading] = useState(false);
 
     const registerIt = async () => {
-        setIsLoading(true);
-        if (router.query.id != "" && router.query.id != undefined) {
-            setReferred(router.query.id);
-            console.log("SDgfsdf");
-            const res = await fetch("https://godot-main-server.vercel.app/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    fname, email, password, referred: router.query.id
-                })
-            })
-            const statusCode = res.status;
-            console.log(statusCode)
-            if (statusCode == 200) {
-                setIsLoading(false);
-                push(`/email?email=${email}&referredBy=${router.query.id}&name=${fname}`);
-            } else {
-                alert("Error while Adding User!!!")
-            }
-        } else if (referred != "" && referred != undefined) {
-            const res = await fetch("https://godot-main-server.vercel.app/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    fname, email, password, referred
-                })
-            })
-            const statusCode = res.status;
-            console.log(statusCode)
-            if (statusCode == 200) {
-                setIsLoading(false);
-                push(`/email?email=${email}&referredBy=${referred}&name=${fname}`);
-            } else {
-                alert("Error while Adding User!!!")
-            }
+        if (email.indexOf(' ') >= 0 || password.indexOf(' ') >= 0 || referred.indexOf(' ') >= 0) {
+            alert("Please remove space from your input field...")
         } else {
-            alert("Referrel url is required to register...")
+            setIsLoading(true);
+            if (router.query.id != "" && router.query.id != undefined) {
+                setReferred(router.query.id);
+                const res = await fetch("https://godot-main-server.vercel.app/register", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        fname, email, password, referred: router.query.id
+                    })
+                })
+                const statusCode = res.status;
+                console.log(statusCode)
+                if (statusCode == 200) {
+                    setIsLoading(false);
+                    push(`/email?email=${email}&referredBy=${router.query.id}&name=${fname}`);
+                } else {
+                    alert("Error while Adding User!!!")
+                }
+            } else if (referred != "" && referred != undefined) {
+                const res = await fetch("https://godot-main-server.vercel.app/register", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        fname, email, password, referred
+                    })
+                })
+                const statusCode = res.status;
+                console.log(statusCode)
+                if (statusCode == 200) {
+                    setIsLoading(false);
+                    push(`/email?email=${email}&referredBy=${referred}&name=${fname}`);
+                } else {
+                    alert("Error while Adding User!!!")
+                }
+            } else {
+                alert("Referrel url is required to register...")
+            }
         }
     }
 
@@ -77,7 +80,7 @@ const Register = () => {
                 <input type="text" placeholder='Email' onChange={(e) => { setEmail(e.target.value) }} />
                 <input type="password" placeholder='Password' onChange={(e) => { setPassword(e.target.value) }} />
                 <input type="text" placeholder='Referral Code' value={router.query.id ? router.query.id : referred} onChange={(e) => { setReferred(e.target.value) }} style={{ marginBottom: '0' }} />
-                <p className="register_btn" style={{marginBottom: "0"}}>Get FREE 10,000 GDOTS on SignUp 🔥🔥🔥</p>
+                <p className="register_btn" style={{ marginBottom: "0" }}>Get FREE 10,000 GDOTS on SignUp 🔥🔥🔥</p>
                 <p className="register_btn">Already a existing user? <span onClick={() => { push("/login"); }}>Login Now!!!</span></p>
                 <button onClick={registerIt}>Register</button>
             </div>
