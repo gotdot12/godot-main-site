f771ee897938e9d2e4e774b29301bf9737581152
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-29 19:39:40 +0530

    Update Register.jsx

--- sections/Register.jsx
+++ sections/Register.jsx
@@ -15,11 +15,14 @@ const Register = () => {
     const [fname, setFname] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const [cpassword, setCPassword] = useState();
     const [referred, setReferred] = useState();
     const [isLoading, setIsLoading] = useState(false);
 
     const registerIt = async () => {
-        if (email.indexOf(' ') >= 0 || password.indexOf(' ') >= 0 || referred.indexOf(' ') >= 0) {
+        if (password != cpassword) {
+            alert("Password doesn't match!!")
+        } else if (email.indexOf(' ') >= 0 || password.indexOf(' ') >= 0 || referred.indexOf(' ') >= 0) {
             alert("Please remove space from your input field...")
         } else {
             setIsLoading(true);
@@ -71,7 +74,7 @@ const Register = () => {
     }
 
     return (
-        <section className={`${styles.paddings}`} id="explore">
+        <section id="explore">
             <div className="loader" style={isLoading ? { visibility: "visible" } : { visibility: "hidden" }} >
                 <span class="spinner spinner--quarter"></span>
             </div>
@@ -83,6 +86,7 @@ const Register = () => {
                 <input type="text" placeholder='Full Name' onChange={(e) => { setFname(e.target.value) }} />
                 <input type="text" placeholder='Email' onChange={(e) => { setEmail(e.target.value) }} />
                 <input type="password" placeholder='Password' onChange={(e) => { setPassword(e.target.value) }} />
+                <input type="text" placeholder='Confirm Password' onChange={(e) => { setCPassword(e.target.value) }} />
                 <input type="text" placeholder='Referral Code' value={router.query.id ? router.query.id : referred} onChange={(e) => { setReferred(e.target.value) }} style={{ marginBottom: '0' }} />
                 <p className="register_btn" style={{ marginBottom: "0" }}>Get FREE 10,000 GDOTS on SignUp 🔥🔥🔥</p>
                 <p className="register_btn">Already a existing user? <span onClick={() => { push("/login"); }}>Login Now!!!</span></p>
