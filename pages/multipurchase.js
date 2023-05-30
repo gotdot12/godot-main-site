6252aa259b1e9f66aeb7b1bd1af3f233404dbd44
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-30 22:01:49 +0530

    Add files via upload

---
+++ pages/multipurchase.js
@@ -0,0 +1,12 @@
+import { MultiPurchase } from '../sections';
+
+const purchase = () => (
+  <div className="purchase-page bg-primary-black overflow-hidden" style={{ width: '100%' }}>
+    <div className="relative">
+      <div className="gradient-03 z-0" />
+      <MultiPurchase />
+    </div>
+  </div>
+);
+
+export default purchase;
