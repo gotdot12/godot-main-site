a62129d7b4d4fe2a39b5e5f682943f471b28ee96
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-06-01 10:47:13 +0530

    Update multipurchase.js

--- pages/multipurchase.js
+++ pages/multipurchase.js
@@ -1,7 +1,7 @@
 import { MultiPurchase } from '../sections';
 
 const purchase = () => (
-  <div className="purchase-page bg-primary-black overflow-hidden" style={{ width: '100%' }}>
+  <div className="purchase-page bg-primary-black overflow-hidden" style={{ width: '100%', minHeight: "100vh" }}>
     <div className="relative">
       <div className="gradient-03 z-0" />
       <MultiPurchase />
