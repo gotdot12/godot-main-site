e69e1a93fed9734393836ead5a6460791d03d51f
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-06-09 14:17:44 +0530

    Add files via upload

---
+++ pages/withdraw.js
@@ -0,0 +1,12 @@
+import { Withdraw } from '../sections';
+
+const withdraw = () => (
+  <div className="purchase-page bg-primary-black overflow-hidden" style={{ width: '100%', minHeight: '100vh' }}>
+    <div className="relative">
+      <div className="gradient-03 z-0" />
+      <Withdraw />
+    </div>
+  </div>
+);
+
+export default withdraw;
